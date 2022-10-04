97cf81567034063b9b30451e2fa663f59bed42a1
marandabreese <marandabreese@cox.net>
2022-10-04 12:51:38 -0700

    Basic Javascript Assignment

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+var stringChallenge = 'Alert! This is a string.';
+var textStuff = 'Really great sentence.';
+var specialCaseString = 'Don\'t sleep on these great characters! Call and say \"Order Now\"' //special characters string
+var concateString = "Help " + "me make potions." //concatenated string
+
+//Assignment Strings//
+var Sent1 = "This is the beginning of the string"
+var Sent2 = " and this is the end of the string"
+
+var cats = "many", catOne = "Ping", catTwo = 'Jenny';
+
+window.alert(stringChallenge); //testing a window alert
+
+document.write(Sent1 + Sent2);//posts to document
