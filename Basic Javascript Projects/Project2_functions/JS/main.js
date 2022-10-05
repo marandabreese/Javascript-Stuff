96ddd5857275e967eebe015fc252958072ef1642
marandabreese <marandabreese@cox.net>
2022-10-05 08:07:49 -0700

    new functions project

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function newFunction() {
+    var str = "This is a test."; //This is the first variable
+    var strPart2 = "<br>This is a new test."; //Second variable, intended to be shown below first
+
+    document.getElementById("cool_text").innerHTML = str+= strPart2; //prints the concatenated string
+}
